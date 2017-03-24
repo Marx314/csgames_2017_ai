b644930d1d308c363785fcb128c27467c073760a
Marc Aubry <maubry@internap.com>
2017-03-24 19:19:00 -0400

    AI yolo

---
+++ src/server.py
@@ -0,0 +1,20 @@
+from twisted.internet import reactor
+from twisted.internet.protocol import Factory
+
+from network.communication import Communication
+from network.online_gateway import OnlineGateway
+from hockey.controller import ControllerGentle
+
+
+class ChatFactory(Factory):
+    def __init__(self):
+        self.users = {}
+        self.online_gateway = OnlineGateway(lambda: ControllerGentle(11, 11), timeout=600, debug=True)
+
+    def buildProtocol(self, addr):
+        return Communication(self.users, self.online_gateway)
+
+
+cf = ChatFactory()
+reactor.listenTCP(8023, cf)
+reactor.run()
